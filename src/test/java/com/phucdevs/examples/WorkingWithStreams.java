574fb088f433c9066ec19f409e9ecb566a10d881
phuc.letan <phuc.letan@messagemedia.com>
2023-04-25 08:50:51 +0700

    Init project java stream api

---
+++ src/test/java/com/phucdevs/examples/WorkingWithStreams.java
@@ -0,0 +1,27 @@
+package com.phucdevs.examples;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class WorkingWithStreams {
+
+    @Test
+    @Disabled
+    void streams() {
+        List<String> names = List.of("Amigoscode", "Alex", "Zara");
+        Stream<String> stream = names.stream();
+
+        Stream<String> namesStream = Stream.of("Amigoscode", "Alex", "Zara");
+
+        long count = stream
+                .limit(2).map(null).sorted(null).dropWhile(null)
+                .count();
+
+        String[] namesArray = {};
+        Arrays.stream(namesArray);
+    }
+}
